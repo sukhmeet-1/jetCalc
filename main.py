bb0b413fd07a113992b67c1feea7eca505d0312f
Rajwinder-Singh-19 <i.am.rajwinder.19@gmail.com>
2025-05-13 04:48:22 +0530

    parsed nasa coefficient yaml file

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from jetcalc!")
+import yaml
+from paths import *
+
+# TODO parse polynomial coefficients for cp and cv calculation of mixture of gaseous species
+
+
+def main(species_name):
+    with open(COEFF_7_NASA_CP_DATA_PATH, "r") as f:
+        data = yaml.safe_load(f)
+
+    print(f"cp / R polynomial coefficients available for the following species:")
+    for species in data["species"]:
+        print(species["name"])
 
 
 if __name__ == "__main__":
-    main()
+    main("CO2")
---
+++ paths.py
@@ -0,0 +1,6 @@
+import os
+
+PROJECT_ROOT = os.getcwd()
+DATA_PATH = os.path.join(PROJECT_ROOT, "data")
+DATA_NASA_PATH = os.path.join(DATA_PATH, "nasa")
+COEFF_7_NASA_CP_DATA_PATH = os.path.join(DATA_NASA_PATH, "gas_specific_heat_coeffs_7.yaml")
