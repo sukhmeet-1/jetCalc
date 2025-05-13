89dd47fe806a17fc3bd5001dcde9b45e9ab2a398
Rajwinder-Singh-19 <i.am.rajwinder.19@gmail.com>
2025-05-13 06:07:25 +0530

    Small documentation

--- cache/STANDARD_GAS_COMPOSITIONS.py
+++ cache/STANDARD_GAS_COMPOSITIONS.py
@@ -1,11 +1,11 @@
 """
 STANDARD_GAS_COMPOSITIONS.py
 
-Standard gaseous mixture compositions are stored in this file in the form of
+Standard gaseous mixture compositions are cached here in the form of
 dictionaries.
 
 The keys are the constituent species' names,
-same as in the 'gas_specific_heat_coeffs_7.yaml' file (from where majority of the thermodynamic data is sourced).
+same as in the 'gas_specific_heat_coeffs_7.yaml' file (from where NASA7 polynomial coefficients are sourced).
 
 The values are the mole fraction of the respective constituent in the mixture
 """
